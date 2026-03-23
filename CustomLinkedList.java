6e1ed132bf9fba927561916a097c5ab8a36bc87a
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-23 12:57:51 +0530

    added private Node class so that internall implementation of LinkedList is hidden from the outside world and all the mutation or access is controlled through LinkedList class to preserve the structural integrity

--- CustomLinkedList.java
+++ CustomLinkedList.java
@@ -1,4 +1,12 @@
 package Complete_Java.Structured_path.LinkedList;
 
 public class CustomLinkedList<T> {
+    private class Node{
+        T data;
+        Node next;
+        Node(T data){
+            this.data = data;
+            this.next = null;
+        }
+    }
 }
