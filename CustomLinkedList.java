fee72bc751f2b6443805818188276779c8b086b8
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-23 13:34:47 +0530

    I’ve implemented insertion operations with strict handling of edge cases and invariants, ensuring O(1) complexity for both head and tail insertions, while abstracting behavior through a consistent API similar to standard collections

--- CustomLinkedList.java
+++ CustomLinkedList.java
@@ -18,4 +18,37 @@ public class CustomLinkedList<T> {
         this.size=0;
     }
 
+    // ----- ADDITION METHODS--------
+    // Default add method behaves like standard collections (adds to end)
+    public void add(T data){
+        addLast(data);
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    // Adds to the beginning of the list: O(1)
+    public void addFirst(T data){
+        Node newNode = new Node(data);
+        if(isEmpty()){
+            head = tail = newNode;
+        }else{
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(T data){
+        Node newNode  = new Node(data);
+        if(isEmpty()){
+            head = tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
 }
